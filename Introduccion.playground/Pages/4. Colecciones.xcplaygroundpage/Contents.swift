418a9e7c1646d168f0ec580de76c94ae13b52d87
hamdsgn <cala_ham12@hotmail.com>
2026-01-14 12:33:18 +0100

    cambios hoy

--- Introduccion.playground/Pages/4. Colecciones.xcplaygroundpage/Contents.swift
+++ Introduccion.playground/Pages/4. Colecciones.xcplaygroundpage/Contents.swift
@@ -6,3 +6,39 @@ import Foundation
 
 var arrayVacio: [Int] = []
 print(arrayVacio)
+print("arrayVacio está vacío? \(arrayVacio.isEmpty)")
+
+var arrayInts: [Int] = [1, 2, 3]
+print(arrayInts)
+
+print("Cuantos elementos tiene arrayInts: \(arrayInts.count)")
+
+// Para añadir un elemento:
+arrayInts.append(0)
+print(arrayInts)
+
+//o bien, podemos añadirlo así:
+arrayInts += [10]
+print(arrayInts)
+
+// Podemos añadir varios elementos de una vez:
+arrayInts += [-1, -2]
+print(arrayInts)
+
+// Para añadir (sin reemplazar) un elemento en una posición específica
+arrayInts.insert(-100, at: 0)
+print(arrayInts)
+
+// Para borrar un elemento en una posición especifica
+arrayInts.remove(at: 0)
+print(arrayInts)
+
+//Acceder a los elementos del array
+let primerElemento = arrayInts[0]
+print("El primer elemento es: \(primerElemento)")
+print("El tercer elemento es: \(arrayInts[2])")
+
+// Modificar un elemento en una posición concreta:
+arrayInts[4] = -500
+print(arrayInts)
+
