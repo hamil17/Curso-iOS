7a2bb3fdbf8681993556b4b8e6084b272bc446ef
hamdsgn <cala_ham12@hotmail.com>
2026-01-16 12:29:38 +0100

    descanso

--- Introduccion.playground/Pages/9. Closures.xcplaygroundpage/Contents.swift
+++ Introduccion.playground/Pages/9. Closures.xcplaygroundpage/Contents.swift
@@ -1,7 +1,14 @@
-//: [Previous](@previous)
-
 import Foundation
 
-var greeting = "Hello, playground"
+// Closures o Funciones anónimas
+func saludar(nombre:String) -> String {
+    return "Hola, \(nombre)!"
+}
 
-//: [Next](@next)
+// Lo que está entre las llaves es un closure
+let saludarClosure = { (nombre:String) -> String in
+        return "Hola, \(nombre)!"
+}
+
+print(saludar(nombre: "Matias"))
+print(saludarClosure("Matias"))
