a6866e8008744e85fad0e31c2e413d7fc5a38ee1
hamdsgn <cala_ham12@hotmail.com>
2026-01-16 14:53:07 +0100

    Final de hoy ejercicio lista de tareas

--- Introduccion.playground/Pages/100. Pruebas.xcplaygroundpage/Contents.swift
+++ Introduccion.playground/Pages/100. Pruebas.xcplaygroundpage/Contents.swift
@@ -1,20 +1,48 @@
 import Foundation
 
-// to do list
-// Array tareas
+// Lista de tareas
 
-class tareas {
-    var nombreTarea: String = ""
-    func agregarTarea() {
-        
+enum Prioridad : Int {
+    case baja = 1
+    case media = 2
+    case alta = 3
+}
+
+enum Estado : String {
+    case pendiente = "Pendiente"
+    case completada = "Completada"
+}
+
+struct Tarea {
+    let id : Int
+    var titulo : String
+    var prioridad : Prioridad
+    var estado : Estado
+    var description: String {
+        return "\(id). \(titulo) - Prioridad: \(prioridad) - Estado: \(estado.rawValue)"
+    }
+}
+
+class GestionTareas {
+    var listaTareas : [Tarea] = []
+    private var contadorId: Int = 0
+    
+    func agregarTarea(titulo:String, prioridad:Prioridad) {
+        contadorId = contadorId + 1
+        let nuevaTarea = Tarea(id: contadorId, titulo: titulo, prioridad: prioridad, estado: .pendiente)
+        listaTareas.append(nuevaTarea)
     }
     func listarTareas() {
-        
+        // Aquí usarás control de flujo y closures
     }
     func completarTarea() {
-        
+        // Aquí buscarás la tarea y cambiarás su estado
     }
     func resumenTareas() {
+        // Operadores y strings para contar pendientes/completadas
         
     }
 }
+
+
+GestionTareas.agregarTarea("Hacer deporte", prioridad: .alta)
